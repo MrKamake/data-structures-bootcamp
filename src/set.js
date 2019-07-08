3ef40c9a19b125f2330f2b47dbd2bd8d57c18637
MrKamake <seongjooon@gmail.com>
2019-07-08 21:40:22 +0900

    complete set.js

--- src/set.js
+++ src/set.js
@@ -2,19 +2,28 @@
  * Complexity: 시간 복잡도에 대해서 생각해보세요.
  */
 
-var Set = function() {
+var Set = function () {
   var set = Object.create(setPrototype);
-  set._storage = null;
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
-setPrototype.add = function(item) {
+setPrototype.add = function (item) {
+  this._storage.push(item);
 };
 
-setPrototype.contains = function(item) {
+setPrototype.contains = function (item) {
+  let check = false;
+  this._storage.forEach(element => {
+    if (element === item) check = true;
+  });
+  return check;
 };
 
-setPrototype.remove = function(item) {
+setPrototype.remove = function (item) {
+  this._storage.forEach((e, i) => {
+    if (e === item) this._storage.splice(i, 1);
+  });
 };
