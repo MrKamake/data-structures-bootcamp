560574562c9b6b8b589518ac801399099a69bd29
MrKamake <seongjooon@gmail.com>
2019-07-08 16:38:25 +0900

    complete likedList.js

--- src/linkedList.js
+++ src/linkedList.js
@@ -2,24 +2,38 @@
  * Complexity: 시간 복잡도에 대해서 생각해보세요.
  */
 
-var LinkedList = function() {
+var LinkedList = function () {
   var list = {};
   list.head = null;
   list.tail = null;
 
-  list.addToTail = function(value) {
+  list.addToTail = function (value) {
+    this.tail = Node(value);
+    if (!this.head) this.head = Node(value);
+    else this.head.next = value;
   };
 
-  list.removeHead = function() {
+  list.removeHead = function () {
+    this.head.value = this.head.next;
+    this.head.next = this.tail.value;
+
+    return this.head.next;
   };
 
-  list.contains = function(target) {
+  list.contains = function (target) {
+    let check = false;
+
+    Object.values(this.head).forEach(element => {
+      if (target === element) check = true;
+    });
+
+    return check;
   };
 
   return list;
 };
 
-var Node = function(value) {
+var Node = function (value) {
   var node = {};
 
   node.value = value;
