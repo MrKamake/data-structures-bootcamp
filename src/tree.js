f7bb5116c3ef69bc0ba149eb4a495f16c6b994f7
MrKamake <seongjooon@gmail.com>
2019-07-08 21:13:46 +0900

    not yet solved final tree problem

--- src/tree.js
+++ src/tree.js
@@ -2,20 +2,28 @@
  * Complexity: 시간 복잡도에 대해서 생각해보세요.
  */
 
-var Tree = function(value) {
+var Tree = function (value) {
   var newTree = {};
   newTree.value = value;
-
-  // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];
+  newTree.addChild = treeMethods.addChild;
+  newTree.contains = treeMethods.contains;
 
   return newTree;
 };
 
 var treeMethods = {};
 
-treeMethods.addChild = function(value) {
+treeMethods.addChild = function (value) {
+  const obj = new Tree(value);
+  this.children.push(obj);
 };
 
-treeMethods.contains = function(target) {
+treeMethods.contains = function (target) {
+  let check = false;
+  if (target === this.children[0].value) check = true;
+  // this.children.forEach(element => {
+  //   if(element.value === target) check = true;
+  // });
+  return check;
 };
