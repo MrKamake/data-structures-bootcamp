fdd64604eb98df6dfb41df5b8b4ad8c7790a6a2a
MrKamake <seongjooon@gmail.com>
2019-07-11 17:23:41 +0900

    Complete binarySearchTree.js

--- src/binarySearchTree.js
+++ src/binarySearchTree.js
@@ -2,5 +2,47 @@
  * Complexity: 시간 복잡도에 대해서 생각해보세요.
  */
 
-var BinarySearchTree = function(value) {
+const BinarySearchTree = function (value) {
+  const bst = Object.create(bstPrototype);
+
+  bst.value = value;
+  bst.left = null;
+  bst.right = null;
+
+  return bst;
+};
+
+const bstPrototype = {};
+
+bstPrototype.insert = function (value) {
+  const currentNode = this;
+  const currentValue = currentNode.value;
+  const newNode = BinarySearchTree(value);
+
+  if (currentValue > newNode.value) {
+    !currentNode.left ? currentNode.left = newNode : currentNode.left.insert(value);
+  }
+  if (currentValue < newNode.value) {
+    !currentNode.right ? currentNode.right = newNode : currentNode.right.insert(value);
+  }
+};
+
+bstPrototype.contains = function (value) {
+  const currentNode = this;
+  const currentValue = currentNode.value;
+
+  if (currentValue === value) return true;
+  if (currentValue > value) {
+    return !currentNode.left ? false : currentNode.left.contains(value);
+  }
+  if (currentValue < value) {
+    return !currentNode.right ? false : currentNode.right.contains(value);
+  }
+};
+bstPrototype.depthFirstLog = function (func) {
+  const currentNode = this;
+
+  func(currentNode.value);
+  if (currentNode.left) currentNode.left.depthFirstLog(func);
+  if (currentNode.right) currentNode.right.depthFirstLog(func);
 };
