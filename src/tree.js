c2c19eb711a366400a44d25863aae613eab836e2
MrKamake <seongjooon@gmail.com>
2019-07-12 17:31:16 +0900

    I re-coded the tree.js

--- src/tree.js
+++ src/tree.js
@@ -2,26 +2,28 @@
  * Complexity: 시간 복잡도에 대해서 생각해보세요.
  */
 
-var Tree = function (value) {
-  var newTree = Object.create(treeMethods);
-
+var Tree = function(value) {
+  var newTree = {};
   newTree.value = value;
-  newTree.children = [];
+  newTree.children = []; 
+  newTree.addChild = treeMethods.addChild;
+  newTree.contains = treeMethods.contains;
 
   return newTree;
 };
 
 var treeMethods = {};
 
-treeMethods.addChild = function (value) {
-  this.children.push(Tree(value));
+treeMethods.addChild = function(value) {
+  this.children.push(new Tree(value));
 };
 
-treeMethods.contains = function (target) {
-  for (let i = 0; i < this.children.length; i++) {
-    if (this.children[i].value === target) return true;
-    else if (this.children[i].children.length && this.children[i].contains(target)) return true;
+treeMethods.contains = function(target) {
+  const currentNode = this;
+  for (let i = 0; i < currentNode.children.length; i++) {
+    if (currentNode.children[i].value === target) return true;
+    else if (currentNode.children[i].children && currentNode.children[i].contains(target)) return true;
   }
-  
+
   return false;
 };
